b98e8681fa82c79abaa8c1cdd140d90d027a9ec7
eriveltondasilva <eriveltondasilva13@gmail.com>
2023-01-16 23:32:04 -0300

    Arquivo js funcionando

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+// '/assets/js/main.js'
+
+// ----- Código para fazer o relógio funcionar -----
+function relogio() {
+    const TELA_HORA = document.querySelector(".js-tela__hora");
+    const DATE = new Date();
+
+    let hour = addZero(DATE.getHours());
+    let min = addZero(DATE.getMinutes());
+    let sec = addZero(DATE.getSeconds());
+
+    let time = hour + ":" + min + ":" + sec;
+
+    TELA_HORA.innerHTML = time;
+};
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    } return i;
+};
+
+setInterval(relogio, 1000);
